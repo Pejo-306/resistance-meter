d258c207cba08543f4b32b12c86d68ac09600408
Petar Nikolov <penikolov23@gmail.com>
2018-07-06 16:28:23 +0300

    Implement functionality to select the measuring range
    The reference resistance's value can be selected by pressing the
    RANGE_SELECT switch of the circuit which will select the next measuring
    range. If the button is pressed on the last range, the selection loops
    back and begins anew (i.e. it selects the first range).

--- ResistanceMeter/ResistanceMeter.ino
+++ ResistanceMeter/ResistanceMeter.ino
@@ -1,20 +1,68 @@
+#include <LiquidCrystal.h>
+
+#define LCD_COLUMNS 16
+#define LCD_ROWS 2
+#define RANGE_1_PIN 2
+#define RANGE_2_PIN 3
+#define RANGE_3_PIN 4
+#define MEASURE_COMMAND_PIN 5
+#define RANGE_SELECT_PIN 6
+#define LCD_D4_PIN 7
+#define LCD_D5_PIN 8
+#define LCD_D6_PIN 9
+#define LCD_D7_PIN 10
+#define LCD_ENABLE_PIN 11
+#define LCD_RS_PIN 12
 #define MEASURING_PIN A0
 
-static const int supplyVoltage = 5;
+// initialize the lcd library
+LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
+
+static const char lcdOhmSign = 222;
+static const char lcdNoSign = 'E';  // should not be represented as a sign
+static const short supplyVoltage = 5;
 static const double voltageConst = supplyVoltage / 1024.0;
-static const double referenceResistance = 10000000;
-static int analogVal;
-static double referenceVoltage;
-static double measuredCurrent;
-static double measuredResistance;
+static const double referenceResistances[] = { 110.0, 9990.0, 10000000.0 };
+static double referenceResistance;
+static const short maxRanges = 3;
+static short currentRange = 2;
 
 void setup()
 {
-    Serial.begin(9600);
+    pinMode(RANGE_1_PIN, OUTPUT);
+    pinMode(RANGE_2_PIN, OUTPUT);
+    pinMode(RANGE_3_PIN, OUTPUT);
+    pinMode(MEASURE_COMMAND_PIN, INPUT);
+    pinMode(RANGE_SELECT_PIN, INPUT);
+    lcd.begin(LCD_COLUMNS, LCD_ROWS);
+
+    selectRange(currentRange);  // select the starting range
 }
 
 void loop()
 {
+    static short analogVal;
+    static double referenceVoltage;
+    static double measuredCurrent;
+    static double measuredResistance;
+    static bool rangeSelectSwitchState;
+    static bool prevRangeSelectSwitchState = false;
+    static bool measureCommandSwitchState;
+    static bool prevMeasureCommandSwitchState = false;
+
+    rangeSelectSwitchState = digitalRead(RANGE_SELECT_PIN);
+    if (rangeSelectSwitchState != prevRangeSelectSwitchState) {
+        if (rangeSelectSwitchState == HIGH) {
+            if (currentRange >= maxRanges)  // reset the range counter
+                currentRange = 0;
+            selectRange(++currentRange);
+        }
+        prevRangeSelectSwitchState = rangeSelectSwitchState;
+    }
+    
+    delay(10);
+    
+    /*
     analogVal = analogRead(MEASURING_PIN);
     referenceVoltage = analogVal * voltageConst;
     measuredCurrent = referenceVoltage / referenceResistance;
@@ -30,5 +78,59 @@ void loop()
     Serial.println(measuredResistance);
 
     delay(100);
+    */
+}
+
+/*
+ * Change the measuring reference resistance
+ * and write the new value to the lcd
+ */
+void selectRange(short range)
+{
+    short *formattedReference;
+    
+    // switch to the selected range on the circuit
+    for (register short pin = 0; pin < maxRanges; ++pin)
+        digitalWrite(RANGE_1_PIN + pin, (pin == range-1) ? HIGH : LOW);
+    
+    // display the new reference resistance
+    referenceResistance = referenceResistances[range-1];
+    formattedReference = lcdFormatParameter(referenceResistance);
+    lcd.clear();
+    lcd.print("Range: ");
+    lcd.print(formattedReference[0]);
+    if (formattedReference[1] != lcdNoSign)
+        lcd.print((char)formattedReference[1]);
+    lcd.print(lcdOhmSign);
+    delete[] formattedReference;
+}
+
+/*
+ * Represent the value of an electical parameter
+ * as a (max) 3-digit value and an lcd symbol
+ * 
+ * The lcdFormats[] array holds values of the lcd's symbol table.
+ * See the documentation of the lcd module for more details.
+ * 
+ * NOTE: do not forget to delete the returned short array after
+ *       having finished working with it
+ */
+short *lcdFormatParameter(double value)
+{
+    static char lcdFormats[] = { 'p', 'n', 234, 'm', 'E', 'k', 'M', 'T' };
+    static short nformats = sizeof(lcdFormats) / sizeof(char);
+    short selectedFormat = nformats / 2;
+    short *formattedResult;
+
+    formattedResult = new short[2];
+    for (; value >= 1000; value /= 1000)
+        selectedFormat++;
+    for (; value < 1 && value != 0; value *= 1000)
+        selectedFormat--;
+    if (selectedFormat < 0 || selectedFormat > nformats - 1);  // TODO: throw some sort of error
+    
+    formattedResult[0] = round(value);
+    formattedResult[1] = lcdFormats[selectedFormat];
+    return formattedResult;
 }
 
